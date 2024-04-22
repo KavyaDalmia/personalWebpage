381e5794bddbebb59eebc5605b22230838a2031d
Kavya Dalmia <dalmia.kavya@gmail.com>
2024-04-21 20:03:20 -0700

    changes

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,6 +1,6 @@
 export const contactsData = {
     email: 'kdalmi01@student.ubc.ca',
-    phone: '+1 236-308-4551',
+    // phone: '+1 236-308-4551',
     address: 'Vancouver, British Columbia, Canada ',
     github: 'https://github.com/KavyaDalmia',
     //facebook: 'https://www.facebook.com/abusaid.riyaz/',
