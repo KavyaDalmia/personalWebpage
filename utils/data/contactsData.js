370594daa7682c697cb5db23c347496dd0b0fedf
Kavya Dalmia <dalmia.kavya@gmail.com>
2024-04-21 19:48:16 -0700

    dataChanges

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'kdalmi01@student.ubc.ca',
+    phone: '+1 236-308-4551',
+    address: 'Vancouver, British Columbia, Canada ',
+    github: 'https://github.com/KavyaDalmia',
+    //facebook: 'https://www.facebook.com/abusaid.riyaz/',
+    linkedIn: 'https://www.linkedin.com/in/kavya-dalmia-94952a1a5/',
+    //twitter: 'https://twitter.com/said7388',
+   // stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+   // devUsername: "said7388"
+}
---
+++ utils/data/educations.js
@@ -0,0 +1,14 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Combined major in Business and Computer Science",
+    duration: "2022 - 2026",
+    institution: "University of British Columbia",
+  },
+  {
+    id: 2,
+    title: "Awards:",
+    duration: "",
+    institution: "Dean's Honor List (Winter 2022), Sauder School of Business International Student Scholarship ($10,000, Winter 2023)",
+  }
+]
