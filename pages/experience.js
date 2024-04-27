d4005eab45ef6ee48f70f3e7444c241b4e23afc6
Kavya Dalmia <dalmia.kavya@gmail.com>
2024-04-27 12:32:56 -0700

    Resolved conflicts by accepting incoming changes

---
+++ pages/experience.js
@@ -0,0 +1,14 @@
+import { personalData } from "@/utils/data/personal-data";
+
+import Experience from "@/app/components/homepage/experience";
+
+const ExperiencePage = () => {
+  return (
+    <div>
+      
+      <Experience />
+    </div>
+  );
+};
+
+export default ExperiencePage;
