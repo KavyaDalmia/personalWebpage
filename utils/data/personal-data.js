767b118213ef9a751756db9a4f286f62bac5aec3
Kavya Dalmia <115609930+KavyaDalmia@users.noreply.github.com>
2024-05-18 16:29:19 -0700

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Kavya Dalmia",
   profile: '/profile.png',
   designation: "University of British Columbia",
-  description: "My name is KAVYA DALMIA. I am a Computer Science and Business student at University of British Columbia. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about solving problems. Fitness is also a huge part of my life, I am a personal trainer where I help people explore the benefits of living an active lifestyle and becoming the best version of themselves. Feel free to reach out to me if you want to have a chat about tech 👩‍💻, computer science 🖥, UBC 📚, fitness🤸‍, food 🥑 or just solve some math 📏 assignments together. ",
+  description: "My name is Kavya Dalmia. I like making cool stuff.",
   email: 'kdalmi01@student.ubc.ca',
   // phone: '+1 236-308-4551',
   address: 'Vancouver, British Columbia ',
